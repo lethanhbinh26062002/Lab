1ccc0f00955659d32f3c3aa1766d4d3b51c61611
lethanhbinh26062002 <lethanhbinh26062002@gmail.com>
2022-01-14 20:57:27 +0700

    Complete: Lab3

--- src/pages/admin/News/News.js
+++ src/pages/admin/News/News.js
@@ -1,9 +1,11 @@
+/* eslint-disable indent */
+/* eslint-disable eol-last */
 import { data } from "../../../data";
 import NavAdmin from "../NavAdmin";
 
 const News = {
-    print() {
-        return /* html */`
+        print() {
+            return /* html */ `
         <div>
           ${NavAdmin.print()}
         </div>
@@ -18,13 +20,12 @@ const News = {
           </div>
           <div class="mt-5 flex lg:mt-0 lg:ml-4">
           <span class="sm:ml-3">
-          <a href="./admin/News/Add">
+          <a href="/admin/News/Add">
               <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <!-- Heroicon name: solid/check -->
                 <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                </svg>
-                
+                </svg>         
                 Add News
               </button>
               </a>
